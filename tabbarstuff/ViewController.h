9cee59c146577b405c47d41a6136342161849e75
Bob Glasser <bobby.glasser@gmail.com>
2014-01-12 21:51:22 -0500

    beginning of tab switching

--- tabbarstuff/ViewController.h
+++ tabbarstuff/ViewController.h
@@ -8,6 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITabBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UITabBarItem *item1;
+@property (weak, nonatomic) IBOutlet UITabBarItem *item2;
+@property (weak, nonatomic) IBOutlet UITabBarItem *item3;
+@property (weak, nonatomic) IBOutlet UITabBar *myTabBar;
+
+@property (weak, nonatomic) IBOutlet UIView *friendsView;
+@property (weak, nonatomic) IBOutlet UIView *tournamentView;
+@property (weak, nonatomic) IBOutlet UIView *statsView;
 
 @end
--- tabbarstuff/ViewController.m
+++ tabbarstuff/ViewController.m
@@ -8,16 +8,49 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+@interface ViewController (){
+    NSArray *tabArray;
+    NSArray *viewArray;
+}
 
 @end
 
 @implementation ViewController
 
+-(void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item
+{
+    //NSLog(@"didSelectItem: %d", item.tag);
+    [self activateView:item.tag];
+}
+
 - (void)viewDidLoad
 {
+    self.myTabBar.delegate = self;
+    UIImage *tabBarBackground = [UIImage imageNamed:@"tabBarBackground.png"];
+    UIImage *tabBarSelectedItem = [UIImage imageNamed:@"tabBarSelectedItem.png"];
+    [self.myTabBar setBackgroundImage:tabBarBackground];
+    [self.myTabBar setSelectionIndicatorImage:tabBarSelectedItem];
+    [self.myTabBar setSelectedItem:self.item1];
+    
+    
+    tabArray = [[NSArray alloc] initWithObjects:_item1,_item2,_item3, nil];
+    viewArray = [[NSArray alloc] initWithObjects:_friendsView, _tournamentView, _statsView, nil];
+    [self activateView:_friendsView.tag];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)activateView:(NSInteger) index{
+    for (NSInteger i=0; i<viewArray.count; i++) {
+        UIView *view = [viewArray objectAtIndex:i];
+        if (i==index) {
+            [view setHidden:NO];
+        }
+        else{
+            [view setHidden:YES];
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
